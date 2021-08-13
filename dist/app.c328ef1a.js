0ce27838927ac5933a28067b0c81e988c4c9e6aa
Adam Romero <adamromero@Adams-MacBook-Pro.local>
2021-08-12 20:45:31 -0700

    Add scroll feature between intro and projects

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -206,7 +206,10 @@ var app = function () {
       triggerMenu: document.getElementById("triggerMenu"),
       projectItems: document.querySelectorAll(".content__overlay"),
       year: document.getElementById("year"),
-      mobileBreakPoint: 768
+      mobileBreakPoint: 768,
+      scrollAboveIntro: false,
+      scrollBelowIntro: false,
+      previousScrollPosition: 0
     },
     init: function init() {
       s = this.settings;
@@ -309,6 +312,30 @@ var app = function () {
         } else {
           s.header.classList.remove("hide");
         }
+
+        var currentScrollPosition = this.window.scrollY + this.window.outerHeight;
+
+        if (currentScrollPosition > s.previousScrollPosition && currentScrollPosition > document.getElementById('intro').offsetHeight && currentScrollPosition < document.querySelector('[data-content="work"]').offsetTop) {
+          if (!s.scrollAboveIntro) {
+            window.scrollTo({
+              top: document.querySelector("[data-content='work']").offsetTop,
+              behavior: "smooth"
+            });
+            s.scrollAboveIntro = true;
+            s.scrollBelowIntro = false;
+          }
+        } else if (currentScrollPosition < s.previousScrollPosition && this.window.scrollY < document.querySelector('[data-content="work"]').offsetTop && this.window.scrollY > document.getElementById('intro').offsetHeight) {
+          if (!s.scrollBelowIntro) {
+            window.scrollTo({
+              top: document.querySelector("#intro"),
+              behavior: "smooth"
+            });
+            s.scrollBelowIntro = true;
+            s.scrollAboveIntro = false;
+          }
+        }
+
+        s.previousScrollPosition = currentScrollPosition;
       });
       s.scrollDown.addEventListener("click", function () {
         window.scrollTo({
@@ -402,7 +429,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43819" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53803" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/app.js
+++ src/app.js
@@ -13,6 +13,9 @@ var app = (function () {
          projectItems: document.querySelectorAll(".content__overlay"),
          year: document.getElementById("year"),
          mobileBreakPoint: 768,
+         scrollAboveIntro: false,
+         scrollBelowIntro: false,
+         previousScrollPosition: 0
       },
 
       init: function () {
@@ -35,8 +38,9 @@ var app = (function () {
          const xhttp = new XMLHttpRequest();
          xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
-               document.getElementById("emailMessage").innerHTML =
-                  this.responseText;
+               document.getElementById(
+                  "emailMessage"
+               ).innerHTML = this.responseText;
             }
          };
          xhttp.open("POST", "/send", true);
@@ -155,12 +159,39 @@ var app = (function () {
             if (
                this.window.scrollY <
                document.getElementById("scrollDown").offsetTop +
-                  document.getElementById("scrollDown").offsetHeight
+               document.getElementById("scrollDown").offsetHeight
             ) {
                s.header.classList.add("hide");
             } else {
                s.header.classList.remove("hide");
             }
+            
+            let currentScrollPosition = this.window.scrollY + this.window.outerHeight;
+            if (currentScrollPosition > s.previousScrollPosition && 
+               currentScrollPosition > document.getElementById('intro').offsetHeight && 
+               currentScrollPosition < document.querySelector('[data-content="work"]').offsetTop) {
+               if (!s.scrollAboveIntro) {
+                  window.scrollTo({
+                     top: document.querySelector("[data-content='work']")
+                        .offsetTop,
+                     behavior: "smooth",
+                  });
+                  s.scrollAboveIntro = true;
+                  s.scrollBelowIntro = false;
+               }
+            } else if (currentScrollPosition < s.previousScrollPosition && 
+               this.window.scrollY < document.querySelector('[data-content="work"]').offsetTop && 
+               this.window.scrollY > document.getElementById('intro').offsetHeight) {
+               if (!s.scrollBelowIntro) {
+                  window.scrollTo({
+                     top: document.querySelector("#intro"),
+                     behavior: "smooth",
+                  });
+                  s.scrollBelowIntro = true;
+                  s.scrollAboveIntro = false;
+               }
+            }
+            s.previousScrollPosition = currentScrollPosition;
          });
 
          s.scrollDown.addEventListener("click", function () {
